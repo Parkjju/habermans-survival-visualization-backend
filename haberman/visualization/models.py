07d785a56d6dbed8fcd7631bfed652dfdd2dc6e4
Parkjju <rudwns3927@gmail.com>
2023-04-28 16:55:10 +0900

    feat: Patient 모델 정의'

--- haberman/visualization/models.py
+++ haberman/visualization/models.py
@@ -1,3 +1,11 @@
+import pandas as pd
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+    age = models.IntegerField()
+    operation_year = models.IntegerField()
+    nb_pos_detected = models.IntegerField()
+    surv = models.IntegerField()
+
+    def __str__(self):
+        return f"Patient {self.pk}"
